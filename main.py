f72ffb27b4c7f366398fd3de3c352bf155e99667
Lin Amber <iltep118666@gmail.com>
2022-09-05 13:40:47 +0300

    1

---
+++ main.py
@@ -0,0 +1,43 @@
+from pprint import pprint
+import json
+import requests
+import os
+
+
+class YandexDisk:
+
+    def __init__(self, token):
+        self.token = token
+
+    def get_headers(self):
+        return {
+            'Content-Type': 'application/json',
+            'Authorization': 'OAuth {}'.format(self.token)
+        }
+
+    def _get_upload_link(self, disk_file_path):
+        upload_url = f"https://cloud-api.yandex.net/v1/disk/resources/upload"
+        headers = self.get_headers()
+        params = {"path": path, "overwrite": "true"}
+        response = requests.get(upload_url, headers=headers, params=params)
+        pprint(response.json())
+        return response.json()
+
+    def upload_file_to_disk(self, disk_file_path, filename):
+        href = self._get_upload_link(disk_file_path=disk_file_path).get("href", "")
+        response = requests.put(href, data=open(filename, 'rb'))
+        response.raise_for_status()
+        if response.status_code == 201:
+            print("Success")
+
+
+if __name__ == '__main__':
+    # тело задачи
+
+    TOKEN = "y0_AgAAAAAucQV2AADLWwAAAADOAh3zzZzkpHG8SEOsmedJo7vXHLAqO7I"
+    path = os.getcwd()
+    ya = YandexDisk(token=TOKEN)
+    print("Текущая директория: ", os.getcwd())
+    path_to_file = os.path.join(path, 'blackmail.txt')
+
+    ya.upload_file_to_disk(path_to_file, 'blackmail.txt')
